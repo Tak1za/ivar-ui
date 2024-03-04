1bb507f1e8226e5e4a77feecc09088ee3df3e9fa
Varun Gupta <varungupta2015135@gmail.com>
2024-03-04 18:27:11 +0530

    added all components for shadcn

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,13 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import {
   ClerkProvider,
+  SignOutButton,
   SignedIn,
   SignedOut,
   UserButton,
 } from "@clerk/clerk-react";
 import { dark } from "@clerk/themes";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -17,27 +19,40 @@ export default function RootLayout() {
   const navigate = useNavigate();
 
   return (
-    <ClerkProvider
-      navigate={navigate}
-      publishableKey={PUBLISHABLE_KEY}
-      appearance={{ baseTheme: dark }}
-    >
-      <header className="header">
-        <div>
-          <div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ClerkProvider
+        navigate={navigate}
+        publishableKey={PUBLISHABLE_KEY}
+        appearance={{
+          baseTheme: dark,
+          elements: {
+            rootBox:
+              "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
+          },
+        }}
+      >
+        <header className="p-2">
+          <div className="flex flex-row justify-between">
             <p>Clerk + React + React Router App</p>
+            <SignedIn>
+              <SignOutButton
+                signOutCallback={() => navigate("/sign-in", { replace: true })}
+              >
+                <Link to="">Sign Out</Link>
+              </SignOutButton>
+            </SignedIn>
+            <SignedOut>
+              <div className="flex flex-row gap-2">
+                <Link to="/sign-in">Sign In</Link>
+                <Link to="/sign-up">Sign Up</Link>
+              </div>
+            </SignedOut>
           </div>
-          <SignedIn>
-            <UserButton afterSignOutUrl="/sign-in" />
-          </SignedIn>
-          <SignedOut>
-            <Link to="/sign-in">Sign In</Link>
-          </SignedOut>
-        </div>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </ClerkProvider>
+        </header>
+        <main className="p-2">
+          <Outlet />
+        </main>
+      </ClerkProvider>
+    </ThemeProvider>
   );
 }
--- src/pages/Index/Index.tsx
+++ src/pages/Index/Index.tsx
@@ -2,16 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function IndexPage() {
   return (
-    <div>
+    <div className="flex flex-col">
       <h1>This is the index page</h1>
       <div>
         <ul>
-          <li>
-            <Link to="/sign-up">Sign Up</Link>
-          </li>
-          <li>
-            <Link to="/sign-in">Sign In</Link>
-          </li>
           <li>
             <Link to="/dashboard">Dashboard</Link>
           </li>
